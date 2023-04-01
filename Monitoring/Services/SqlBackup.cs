8243a2bb4844883f6ecefdcb58c8d328a71a15f3
detf0r <vanek326707@yandex.ru>
2023-04-02 02:07:54 +0700

    добавил скобку

--- Monitoring/Services/SqlBackup.cs
+++ Monitoring/Services/SqlBackup.cs
@@ -14,46 +14,47 @@ namespace Monitoring.Services
     public static class SqlBackup
     {
 
-       
-            public static void backup_db(IResourceService database)
-            {
-                List<Monitoring.Models.Parameter> listOfParam = database.GetAllParameters();
-                string backupPath = create_backupPath();
-                
 
-                using (var db = new DataContext())
+        public static void backup_db(IResourceService database)
+        {
+            List<Monitoring.Models.Parameter> listOfParam = database.GetAllParameters();
+            string backupPath = create_backupPath();
+
+
+            using (var db = new DataContext())
+            {
+                //SqlResourceService.GetAllParameters();
+
+                var connection = db.Database.Connection as SqlConnection;
+                connection.Open();
+                if (connection != null && connection.State == System.Data.ConnectionState.Open)
                 {
-                    //SqlResourceService.GetAllParameters();
-
-                    var connection = db.Database.Connection as SqlConnection;
-                    connection.Open();
-                    if (connection != null && connection.State == System.Data.ConnectionState.Open)
-                    {
-                        Directory.CreateDirectory(backupPath);
-                        Console.WriteLine("backup");
-                        string query = $"BACKUP DATABASE [{connection.Database}] TO DISK = \'{backupPath}\\{DateTime.Now.Hour}_{DateTime.Now.Minute}.bak\'";
-                        db.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction, query);
-                    }
-                    connection.Close();
+                    Directory.CreateDirectory(backupPath);
+                    Console.WriteLine("backup");
+                    string query = $"BACKUP DATABASE [{connection.Database}] TO DISK = \'{backupPath}\\{DateTime.Now.Hour}_{DateTime.Now.Minute}.bak\'";
+                    db.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction, query);
                 }
-                //using (var streamWriter = new StreamWriter(@".\Res_mapping.csv", false, Encoding.GetEncoding(1251)))
-                //{
-                //    var columns = Area.Param.Cast<DataColumn>();
-                //    streamWriter.WriteLine(string.Join(";", columns));
-
-                //    foreach (DataRow row in Adress_for_mapping.Rows)
-                //    {
-                //        streamWriter.WriteLine(string.Join(";", row.ItemArray));
-                //    }
-                //}
+                connection.Close();
             }
-            static string create_backupPath()
-            {
-                string backupPath = "C:\\\\DB_BACKUPS";
-                backupPath += $"\\[{DateTime.Today.Year.ToString()}]\\[{DateTime.Today.Month.ToString()}]\\[{DateTime.Today.Day.ToString()}]";
+            //using (var streamWriter = new StreamWriter(@".\Res_mapping.csv", false, Encoding.GetEncoding(1251)))
+            //{
+            //    var columns = Area.Param.Cast<DataColumn>();
+            //    streamWriter.WriteLine(string.Join(";", columns));
+
+            //    foreach (DataRow row in Adress_for_mapping.Rows)
+            //    {
+            //        streamWriter.WriteLine(string.Join(";", row.ItemArray));
+            //    }
+            //}
+        }
+        static string create_backupPath()
+        {
+            string backupPath = "C:\\\\DB_BACKUPS";
+            backupPath += $"\\[{DateTime.Today.Year.ToString()}]\\[{DateTime.Today.Month.ToString()}]\\[{DateTime.Today.Day.ToString()}]";
 
 
-                return backupPath;
-            }
-        
+            return backupPath;
+        }
+
     }
+}
