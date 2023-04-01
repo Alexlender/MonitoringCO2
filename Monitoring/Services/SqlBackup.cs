3273dce42f6f6749f77f7ab7d38b4c54bb3fde85
detf0r <vanek326707@yandex.ru>
2023-04-01 23:01:54 +0700

    теперь бэкапы точно работает бля буду зуб даю мамой клянусь вот тебе крест

--- Monitoring/Services/SqlBackup.cs
+++ Monitoring/Services/SqlBackup.cs
@@ -14,20 +14,22 @@ namespace Monitoring.Services
           
             using (var db = new DataContext())
             {
-                var connection = db.Database.Connection as SqlConnection;
                 string backupPath = create_backupPath();
+                var connection = db.Database.Connection as SqlConnection;
+                connection.Open();
                 if (connection != null && connection.State == System.Data.ConnectionState.Open)
                 {
-                    Console.WriteLine("Button test");
-                    string query = $"BACKUP DATABASE [{connection.Database}] TO DISK = \'{backupPath}\'";
+                    Directory.CreateDirectory(backupPath);
+                    Console.WriteLine("backup");
+                    string query = $"BACKUP DATABASE [{connection.Database}] TO DISK = \'{backupPath}\\{DateTime.Today.Minute}_{DateTime.Today.Second}.bak\'";
                     db.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction, query);
                 }
-                
+                connection.Close();
             }
         }
         static string create_backupPath()
         {
-            string backupPath= ".\\\\DataContextBackup";
+            string backupPath= "C:\\\\DB_BACKUPS";
             backupPath += $"\\[{DateTime.Today.Year.ToString()}]\\[{DateTime.Today.Month.ToString()}]\\[{DateTime.Today.Day.ToString()}]";
 
 
